7e039f059f7f1ffa2b901301a7e53434b1e20fa2
carnarez <11967125+carnarez@users.noreply.github.com>
2022-06-18 10:38:21 +0200

    Fixed setup.py

--- ddlwheel/engines/__init__.py
+++ ddlwheel/engines/__init__.py
@@ -1,9 +1,6 @@
 """Database-flavoured utils and functions.
 
-In particular, three functions are expected in each engine module:
-* `fetch_objects()` listing all database objects to describe.
-* `fetch_ddl()` querying the definition of a single object (DDL) which will later be
-  parsed to extract direct parents.
-* `fetch_details()` which does all the work, that is, leverages the previous functions
-  to "fully" describe all objects in the database(s).
+In particular, one function is expected in each engine module: `fetch_details()` which
+does all the work, that is, leverages the others database-flavoured functions to "fully"
+describe all objects present in the database(s).
 """
--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ import setuptools  # type: ignore
 setuptools.setup(
     author="carnarez",
     description="Query and parse SQL DDL, and extract lineage.",
-    install_requires=["sqlparse"],
+    install_requires=["sqlparse", "tqdm"],
     name="ddlwheel",
-    packages=["ddlwheel"],
+    packages=setuptools.find_packages(),
     package_data={"ddlwheel": ["py.typed"]},
     url="https://github.com/carnarez/ddlwheel",
     version="0.0.1",
